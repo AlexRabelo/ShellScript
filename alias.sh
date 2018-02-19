fca2856fb62b428e25f18b20a60ff777b6029ebe
Alex Rabelo <alexrabelo@gmail.com>
2018-02-19 15:55:44 -0300

    Create alias.sh

---
+++ alias.sh
@@ -0,0 +1,13 @@
+# Define new aliases
+alias tm='clear & tail -f /var/log/messages'
+alias tmm='clear & tail -f /var/log/maillog'
+alias tms='clear & tail -f /var/log/secure'
+
+# Configuracoes personalizadas
+# Opcoes personalizadas do history
+HISTSIZE=10000
+HISTFILESIZE=10000
+HISTCONTROL=ignoredups
+HISTIGNORE="pwd:ls:ll:ls -ltr:"
+export readonly HISTTIMEFORMAT="%d-%b-%Y %T -> "
+export readonly HISTSIZE HISTFILESIZE HISTCONTROL HISTIGNORE
